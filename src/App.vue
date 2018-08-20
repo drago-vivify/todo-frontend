4da3ac1931b41cbf86493dd1ff749c976a98c8b7
Drago <drago.s@vivifyideas.com>
2018-08-20 17:28:34 +0200

    Initial commit - basic VueJS template

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="container">
+      some bootstrapped text
+      <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
